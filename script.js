274f1ad1266ce3ed44386434784471de96bd792c
Koustuv Sinha <koustuvsinha@hotmail.com>
2014-12-30 10:33:15 +0530

    Added Wikia Support

--- script.js
+++ script.js
@@ -149,8 +149,9 @@ function QuickWiki() {
           });
           $('#wiki-close, #wiki-open').live('click', quickwiki_close);
 
+            
           // load the content from the link into the QuickWiki "window"
-          loadContent(window.location.protocol+'//'+window.location.host+$(this).attr('href')+' #content');
+          loadContent(window.location.protocol+'//'+window.location.host+$(this).attr('href')+' ' + getIdTag());
 
           // make the QuickWiki "window" draggable
           title
@@ -247,15 +248,20 @@ function loadContent(url) {
   var content = $('#wiki-content');
   $('#wiki-title-info').html(get_loader_html());
   content.load(url, function (res, status, xhr) {
+    console.log(content);  
     $('#wiki-title-info').remove('');
     $("#wiki-title").prepend('<div id="wiki-title-info"></div>');
     if (status != 'error') {
       var heading = content
-        .find('div#content')
+        .find('div'+getIdTag())
         .css({ 'margin-left': '0px' })
         .find('h1:eq(0)')
         .text();
       $('#wiki-title-info').html('QuickWiki: ' + heading);
+      if(getHost()==="wikia") {
+          content.find('div' +getIdTag())
+                 .css({ 'margin-right' : '0px', 'margin-top' : '5px'});
+      }
       content.find('a[href^=/wiki/]').each(function () {
         // tips on hover
         var link_title = $(this).attr('title');
@@ -263,9 +269,12 @@ function loadContent(url) {
 
         $(this).unbind('click').click(function (e) {
           e.preventDefault();
-          loadContent(window.location.protocol+'//'+window.location.host+$(this).attr('href')+' #content');
+          loadContent(window.location.protocol+'//'+window.location.host+$(this).attr('href')+' ' + getIdTag());
         });
       });
+      //always render scroll to top, specially added to handle in modal window navigations
+      $(this).scrollTop(0);    
+      
     }
   });
 }
@@ -298,4 +307,29 @@ function get_loader_html() {
       '<div id="followingBallsG_3" class="followingBallsG"></div>' +
       '<div id="followingBallsG_4" class="followingBallsG"></div>' +
     '</div>';
+}
+
+//check whether the url is from wikia or wikipedia
+
+function getHost() {
+  var host = window.location.host;
+  if(host.indexOf("wikia.com") > -1) 
+      return "wikia";
+  if(host.indexOf("wikipedia.org") > -1)
+      return "wikipedia";
+}
+
+//set appropriate content id for url type
+
+function getIdTag() {
+  var idTag = '';     
+  if(getHost()==="wikia") {
+      //the link is from wikia.com, set appropriate id tag
+      idTag = "#WikiaMainContentContainer";
+  }
+  else if(getHost()==="wikipedia") {
+      //the link is from wikipedia.org, set appropriate id tag
+      idTag = "#content";
+  }
+  return idTag;
 }
